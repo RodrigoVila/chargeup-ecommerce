a8496646f635df464f2c2dbdd8f415521b6544ca
Xiantek <jpmnet2050@gmail.com>
2021-12-26 20:31:48 +0100

    26_12_c

--- src/components/admin/AdminUserNav.tsx
+++ src/components/admin/AdminUserNav.tsx
@@ -29,11 +29,6 @@ return (
               {/* Mobile menu button*/}
               <div className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                 <span className="sr-only">Open main menu</span>
-                {open ? (
-                  <div className="block h-3 w-3" aria-hidden="true" />
-                ) : (
-                  <div className="block h-3 w-3" aria-hidden="true" />
-                )}
               </div>
             </div>
             <div className="flex-1 flex items-center justify-center">
