c32ad5d87d7522af9674c13536363c583e15665c
Xiantek <jpmnet2050@gmail.com>
2021-12-26 18:42:58 +0100

    26_12

--- src/components/admin/AdminProductDetails.tsx
+++ src/components/admin/AdminProductDetails.tsx
@@ -1,25 +1,29 @@
 import React, { useState } from 'react'
-import current_product from './AdminProductsListJP'
+import current_product from './AdminProducts'
 
 
 const products = [
   {
     id: 1,
     name: 'Nueces Pecan BIO origen Francia, Paris.',
-    description: 'Nueces Pecan BIO origen Francia, Paris. Batch#02665 .Proovedor: Catalunya Seeds (Barcelona)',
+    description: 'Nueces Pecan BIO origen Francia, Paris. Batch#02665. Proovedor: Catalunya Seeds (Barcelona)',
     imageSrc: 'https://cdn.shopify.com/s/files/1/1699/9375/files/la-nuez-pecan-ayuda-a-adelgazar_large.jpg?v=1499651301',
     imageAlt: "Producto",
     price: '$18.55',
     stock: 10,
-    unidad: "Kg",
+    unit: "Kg",
     last_update:"20/12/2021 12:29pm"
   },
   // More products...
 ]
 
+const AdminProductDetails = (props) => {
+  const [isEdit, setIsEdit] = useState(false)
 
-function AdminProductJP(props) {
-  
+  const handleEdit = () => setIsEdit(true)
+
+  const handleSaveChanges = () => setIsEdit(false)
+
   return (
       <div className="bg-white my-2">
         <div className="max-w-2xl mx-auto py-2 px-4 sm:py-8 sm:px-6 rounded-xl lg:max-w-xl lg:px-8 shadow-lg">
@@ -38,13 +42,14 @@ function AdminProductJP(props) {
                 <div className="mt-4 flex justify-between">
                   <div>
                     <h3 className="text-sm text-gray-700 font-bold border-solid border-b border-zinc-300 py-2">Nombre de Producto:</h3>
-                        <div className='text-gray-500 my-2'> {product.name} </div>
+                    {isEdit ? <input className="border-2 border-green-500 py-1 px-2" type="text" value={product.name} onChange={()=>{}}/> : <div className='text-gray-500 my-2'> {product.name} </div> }
+                        
 
                     <h3 className="text-sm text-gray-700 font-bold border-solid border-b border-zinc-300 py-2">Stock:</h3>
-                      <div className="text-gray-500 my-2"> {product.stock}{ product.unidad}</div>
+                      <div className="text-gray-500 my-2"> {product.stock}{ product.unit}</div>
 
                     <h3 className="text-sm text-gray-700 font-bold border-solid border-b border-zinc-300 py-2">Precio:</h3>  
-                      <div className="text-gray-500 my-2"> <span className='font-bold'>{product.price}</span> ({ product.unidad})</div>
+                      <div className="text-gray-500 my-2"> <span className='font-bold'>{product.price}</span> ({ product.unit })</div>
 
                     <h3 className="text-sm text-gray-700 font-bold border-solid border-b border-zinc-300 py-2">Descripcion:</h3>  
                       <div className="text-gray-500 my-2"> { product.description}</div>
@@ -56,7 +61,7 @@ function AdminProductJP(props) {
               </div>
             ))}
             <div className='object-center align-middle border-t border-solid border-gray200 my-2 py-4'>
-              <button className='text-gray-200 hover:text-gray-50 bg-violet-700 rounded-lg shadow-xl shadow-violet-700/30 py-2 px-4 mr-8'>Editar Producto</button>
+              <button className={`${isEdit ? "bg-pink-700" : "bg-violet-700"} text-gray-200 hover:text-gray-50 rounded-lg shadow-xl shadow-violet-700/30 py-2 px-4 mr-8`} onClick={isEdit ? handleSaveChanges : handleEdit}>{isEdit ? "Guardar Cambios" : "Editar Producto"}</button>
               <button className='text-gray-200 hover:text-gray-50 bg-violet-700 rounded-lg shadow-xl shadow-violet-700/30 py-2 px-4 mr-4'> Eliminar producto</button>
             </div>
 
@@ -66,4 +71,4 @@ function AdminProductJP(props) {
   )
 }
 
-export default AdminProductJP;
+export default AdminProductDetails;
--- src/components/admin/AdminUserNav.tsx
+++ src/components/admin/AdminUserNav.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Fragment, useState } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
@@ -13,15 +13,13 @@ const navigation = [
     { name: 'Estadisticas', href: '#', current: false },
   ]
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
+function classNames(...classes) { return classes.filter(Boolean).join(' ') }
 
-function AdminUserNav (props) {
-    const activepage = props.activep
-    const isactive = ''
-  //  if (activepage === activ)
-   // { navigation.map((item) => (  if (activepage === item.name) isactive = true )) }
+const AdminUserNav = ({setActivePage, activePage}) => {
+
+  const handleClick = (item:string):any => {
+    setActivePage(item)
+  }
 
 return (
     <Disclosure as="nav" className="bg-violet-900">
@@ -61,15 +59,16 @@ return (
                     <button
                       key={item.name}
                       name= {item.name}
-                      onClick={() => props.setPage(item.name) }
+                      // onClick={() => setActivePage(item.name)}
+                      onClick={() => handleClick(item.name)}
                       // onClick={() => props.setMkactive(true) }
                       
 
                       className={classNames(
-                        isactive ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                        activePage ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                         'px-3 py-2 rounded-md text-sm font-medium'
                       )}
-                      aria-current={isactive ? 'page' : undefined}
+                      aria-current={activePage ? 'page' : undefined}
                     >
                        { /* activepage es igual a iten.name / falta hacer el IF para que quede marcada (cambiar el background) EJ: Clientes*/}
                       {item.name}
@@ -79,62 +78,7 @@ return (
               </div>
             </div>
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-
-              {/* Profile dropdown */}
-              <Menu as="div" className="ml-3 relative bg-gray-800 ">
-                <div>
-                  <Menu.Button className="text-lime-200 bg-stone-500 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                    <span className="sr-only decoration-gray-200 hover:text-lime-200">Open user menu</span>
-                    <img
-                      className="h-3 w-3 rounded-full"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt=""
-                    />
-                  </Menu.Button>
-                </div>
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
-                >
-                  <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <Menu.Item>
-                      {({ active }) => (
-                        <a
-                          href="#"
-                          className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                        >
-                          Your Profile
-                        </a>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <a
-                          href="#"
-                          className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                        >
-                          Settings
-                        </a>
-                      )}
-                    </Menu.Item>
-                    <Menu.Item>
-                      {({ active }) => (
-                        <a
-                          href="#"
-                          className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                        >
-                          Sign out
-                        </a>
-                      )}
-                    </Menu.Item>
-                  </Menu.Items>
-                </Transition>
-              </Menu>
+              
             </div>
           </div>
         </div>
