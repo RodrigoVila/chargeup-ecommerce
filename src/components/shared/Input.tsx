7a0385ff2302d8c3791430bde0ae5dca0d248907
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2023-07-10 21:09:09 +0200

    refactor(main): Refactor input component

--- src/components/shared/Input.tsx
+++ src/components/shared/Input.tsx
@@ -1,47 +1,37 @@
-import React, { FC, ChangeEvent } from 'react';
+import React, { FC, ChangeEvent, InputHTMLAttributes } from 'react';
 import { PencilIcon } from '@heroicons/react/24/outline';
 
-type Props = {
-  label: string;
-  name: string;
-  type: string;
-  value?: string | null;
-  disabled?: boolean;
-  placeholder?: string;
+type Props = InputHTMLAttributes<HTMLInputElement> & {
+  label:string;
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
   setEditingField?: (field: string) => void;
 };
 
 const Input: FC<Props> = ({
   label,
-  name,
-  type,
-  value,
-  disabled = false,
-  placeholder = '',
   onChange,
   setEditingField,
+  ...rest
 }) => {
   return (
     <div className="w-full mb-4">
-      <label className="block mb-1 text-lg" htmlFor={name}>
+      <label className="block mb-1 text-lg" htmlFor={rest.name}>
         {label}
       </label>
       <div className="flex">
         <input
           className={`${
-            disabled && 'bg-slate-300 text-slate-600'
+            rest.disabled && 'bg-slate-300 text-slate-600'
           } focus:shadow-outline w-full appearance-none rounded border px-3 py-2 leading-tight shadow focus:outline-none`}
-          id={name}
-          name={name}
-          type={type}
-          value={value}
+          id={rest.name}
+          name={rest.name}
+          type={rest.type}
+          value={rest.value}
           onChange={onChange}
-          disabled={disabled}
-          placeholder={placeholder}
+          {...rest}
         />
         {setEditingField && (
-          <button className="w-6 h-6 ml-2 cursor-pointer" onClick={() => setEditingField(name)}>
+          <button className="w-6 h-6 ml-2 cursor-pointer" onClick={() => setEditingField(rest.name)}>
             <PencilIcon />
           </button>
         )}
