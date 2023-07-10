e8b41ecbbb85d080f3d5d398480e60abad338275
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2023-07-10 22:13:35 +0200

    refactor(main): Refactor button component

--- src/components/main/Buttons/Button.tsx
+++ src/components/main/Buttons/Button.tsx
@@ -1,17 +1,18 @@
-import { FormEvent, ReactNode } from 'react';
-import { colors } from '@constants/colors';
+import { ButtonHTMLAttributes, FormEvent, ReactNode } from 'react';
 import Spinner from '@shared/Spinner';
+import { twMerge } from 'tailwind-merge';
 
-interface Props {
+type ButtonProps = Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'type' | 'className'> & {
   title: string;
   leftIconComponent?: ReactNode;
   rightIconComponent?: ReactNode;
   isSubmit?: boolean;
   onClick: (e: FormEvent<HTMLButtonElement>) => void;
   type?: 'filled' | 'outlined';
-  className?: string;
   loading?: boolean;
-}
+  className?: string;
+  labelClassName?: string;
+};
 
 const Button = ({
   title,
@@ -20,34 +21,39 @@ const Button = ({
   isSubmit,
   onClick,
   type = 'filled',
-  className = '',
   loading = false,
-}: Props) => {
+  className= "",
+  labelClassName = '',
+  ...rest
+}: ButtonProps) => {
   const baseStyles =
-    'z-20 flex w-full cursor-pointer items-center justify-center rounded-md py-2 text-white py-2 px-4 text-sm';
+    'group z-20 flex w-full cursor-pointer items-center justify-center rounded-md py-2 py-2 px-4';
 
-  const typeStyles = type === 'outlined' ? 'border border-black text-black hover:text-white hover:bg-black' : 'bg-mainPurple hover:bg-fuchsia-600';
+  const typeStyles =
+    type === 'outlined'
+      ? 'border border-black hover:bg-black'
+      : 'bg-mainPurple hover:bg-fuchsia-600';
 
-  const textStyles = type === 'outlined' ? 'text-black' : 'text-white';
+  const textStyles =
+    type === 'outlined' ? 'text-black group-hover:text-white text-sm' : 'text-white text-sm';
 
   const iconStyle = 'h-full mx-1';
 
   return loading ? (
-    <button className={`${baseStyles} bg-gray-400 text-gray-200`} onClick={onClick} disabled>
+    <button className={twMerge('bg-gray-400 text-gray-200', baseStyles)} onClick={onClick} disabled>
       <Spinner />
     </button>
   ) : (
     <button
-      className={`${baseStyles} ${typeStyles} ${className}`}
+      className={twMerge(baseStyles, typeStyles, className)}
       onClick={onClick}
       type={isSubmit ? 'submit' : 'button'}
       role={isSubmit ? 'link' : 'button'}
+      {...rest}
     >
-      <>
-        {leftIconComponent && <div className={iconStyle}>{leftIconComponent}</div>}
-        <p className={`${textStyles} h-full w-full py-1 px-4`}>{title}</p>
-        {rightIconComponent && <div className={iconStyle}>{rightIconComponent}</div>}
-      </>
+      {leftIconComponent && <div className={iconStyle}>{leftIconComponent}</div>}
+      <p className={twMerge('h-full w-full py-1 px-4', textStyles, labelClassName)}>{title}</p>
+      {rightIconComponent && <div className={iconStyle}>{rightIconComponent}</div>}
     </button>
   );
 };
