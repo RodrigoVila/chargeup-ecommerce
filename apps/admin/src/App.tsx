180403ceab60e17ff7caf7803124487738b7acc6
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2024-08-25 16:37:21 +0200

    feat(admin): Add toast package to admin app

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,5 +1,7 @@
 import { RouterProvider } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ToastContainer } from '@packages/toast-notifications'
+import '@packages/toast-notifications/styles.css'
 
 import { router } from './router'
 
@@ -9,6 +11,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ToastContainer />
     </QueryClientProvider>
   )
 }
