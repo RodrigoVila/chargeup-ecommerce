e87e52af0034935afae7f5eb25daae06468d0579
Rodrigo Vila <rodrigo.s.vila@gmail.com>
2024-08-25 03:32:08 +0200

    fix(packages): Add types export to missing packages

--- packages/button/vite.config.ts
+++ packages/button/vite.config.ts
@@ -23,16 +23,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    // coverage: {
-    //   reporter: ['clover', 'lcov', 'text', 'json', 'html'],
-    //   all: true,
-    //   include: ['src'],
-    //   exclude: ['src/index.ts'],
-    //   statements: 80,
-    //   branches: 80,
-    //   functions: 80,
-    //   lines: 80,
-    // },
     restoreMocks: true,
   },
 })
